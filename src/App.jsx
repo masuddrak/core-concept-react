f315a57cfd7ffceb3fadaa86ca35e5f506e9fbaa
masud <masudranaacb@gmail.com>
2024-03-08 12:29:52 +0600

    react rendering map

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Conditional from './Conditional'
+import Singer from './Singer'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const singers=[
+    {id:1,name:"salman",age:25},
+    {id:2,name:"salman",age:20},
+    {id:3,name:"salman",age:35},
+    {id:4,name:"salman",age:45},
+    {id:5,name:"salman",age:65},
+  ]
 
-  return (
-    <>
-      <h1>hello react js</h1>
-      <Conditional isDone={true} taks="html"></Conditional>
-      <Conditional isDone={true} taks="css"></Conditional>
-      <Conditional isDone={false} taks="javascrip"></Conditional>
-    </>
-  )
+  return (<>
+      <h1>Hello React js</h1>
+     {
+      singers.map(singer=><Singer singer={singer}></Singer>)
+     }
+  </>)
 }
 
 export default App
---
+++ src/Singer.jsx
@@ -0,0 +1,10 @@
+function Singer({singer}){
+    console.log(singer)
+    return (<>
+
+    <h4>Name:{singer.name}</h4>
+    <h4>age:{singer.age}</h4>
+    
+    </>)
+}
+export default Singer
